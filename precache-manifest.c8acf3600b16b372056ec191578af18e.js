d50489df6e92984d965285da49c9c018a139a838
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-06 14:12:05 -0400

    Updates

---
+++ precache-manifest.c8acf3600b16b372056ec191578af18e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c496183b77f83642675d8c034d9c84d2",
+    "url": "/reactCounter/index.html"
+  },
+  {
+    "revision": "25cd21588e8d47875666",
+    "url": "/reactCounter/static/css/main.86de03ce.chunk.css"
+  },
+  {
+    "revision": "d217ce698b2c1a950833",
+    "url": "/reactCounter/static/js/2.f1a265f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCounter/static/js/2.f1a265f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cd21588e8d47875666",
+    "url": "/reactCounter/static/js/main.87c05957.chunk.js"
+  },
+  {
+    "revision": "741a884ef367047d4abb",
+    "url": "/reactCounter/static/js/runtime-main.c2b4ca95.js"
+  },
+  {
+    "revision": "aa5b4cf70969d624d7d52cbe9942bdf6",
+    "url": "/reactCounter/static/media/carnival.aa5b4cf7.jpg"
+  }
+]);
